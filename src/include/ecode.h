f74efb0368cc9c8c4a6c95ba493a7b29b38b4784
bratpeki <pkatic2003@gmail.com>
2025-12-21 23:56:21 +0100

    Added arg parsing

--- src/include/ecode.h
+++ src/include/ecode.h
@@ -9,7 +9,9 @@
 
 typedef enum {
 	ECODE_SUCCESS = 0,
-	ECODE_NOARG = 1 /* Insufficient arguments passed */
+	ECODE_NOARG = 1, /* Insufficient arguments passed */
+	ECODE_TESTFAIL = 2, /* Some test in `tests` failed */
+	ECODE_BADARG = 3 /* CLI args not passed right */
 } ecode;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,33 +4,91 @@
 
 #include "./include/ecode.h"
 #include "./include/printutil.h"
+#include "./include/strutil.h"
 #include "./include/bool.h" /* TODO: There's probably gonna be boolean flags */
 
 int main( int argc, char** argv ) {
 
 	/* Decls */
 
+	const char* filepath = NULL;
+	const char* outpath  = NULL;
+	const char* csspath  = NULL;
+
 	int i;
 
-	const char* filename; /* CLI arg: ADOC file to be converted */
-
-	/* Code */
-
-	/*
-	 * The first argument is always the program itself,
-	 * the rest are flags and the file
-	 *
-	 * We want at least two args, those being the program and ADOC file
-	 */
+	/* We need at least the program name and one argument */
 
 	if ( argc < 2 ) {
 		printutil("No arguments specified!\n", PRINTUTIL_ERROR);
 		return ECODE_NOARG;
 	}
 
-	for ( i = 0; i < argc; i++ ) {
-		puts(argv[i]);
+	/* Start loop at 1 to skip the program name (argv[0]) */
+
+	for ( i = 1; i < argc; i++ ) {
+
+		/* Handle Output Path Flag */
+
+		if ( STREQ(argv[i], "-o") ) {
+
+			if ( outpath != NULL ) {
+				printutil("Duplicate -o flag detected!\n", PRINTUTIL_ERROR);
+				return ECODE_BADARG;
+			}
+
+			if ( i + 1 >= argc ) {
+				printutil("-o requires an output path!\n", PRINTUTIL_ERROR);
+				return ECODE_BADARG;
+			}
+
+			outpath = argv[++i]; /* Store next arg and skip it in the loop */
+
+		}
+
+		/* Handle CSS Path Flag */
+		else if ( STREQ(argv[i], "-c") ) {
+
+			if ( csspath != NULL ) {
+				printutil("Duplicate -c flag detected!\n", PRINTUTIL_ERROR);
+				return ECODE_BADARG;
+			}
+
+			if ( i + 1 >= argc ) {
+				printutil("-c requires a CSS path!\n", PRINTUTIL_ERROR);
+				return ECODE_BADARG;
+			}
+
+			csspath = argv[++i]; /* Store next arg and skip it in the loop */
+
+		}
+
+		/* Handle Positional Argument (The ADOC file) */
+
+		else {
+
+			if ( filepath != NULL ) {
+				printutil("Too many positional arguments!\n", PRINTUTIL_ERROR);
+				return ECODE_BADARG;
+			}
+
+			filepath = argv[i];
+
+		}
+
 	}
+
+	/* Validation: Ensure we actually got an input file */
+	if ( filepath == NULL ) {
+		printutil("No input file specified!\n", PRINTUTIL_ERROR);
+		return ECODE_NOARG;
+	}
+
+	/* Print Results */
+
+	printutil("FILEPATH: ", PRINTUTIL_INFO); printf("%s\n", filepath);
+	if (outpath) { printutil("OUTPATH:  ", PRINTUTIL_INFO); printf("%s\n", outpath); }
+	if (csspath) { printutil("CSSPATH:  ", PRINTUTIL_INFO); printf("%s\n", csspath); }
 
 	return ECODE_SUCCESS;
 
